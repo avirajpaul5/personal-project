593f0fecece71f652691b9500a88065245601d7e
Aviraj Paul <96339559+avirajpaul5@users.noreply.github.com>
2025-04-08 09:48:01 +0530

    Make ui changes to contact section

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,9 +1,14 @@
+import { ExternalLink, Inbox } from "lucide-react";
+
 export default function Contact() {
   return (
     <div className="space-y-4 p-6">
       <h2 className="text-2xl font-bold dark:text-white">Get in Touch</h2>
       <div className="space-y-2 dark:text-gray-300">
-        <a href="mailto:avirajpaul5@gmail.com">📧 Email</a>
+        <a href="mailto:avirajpaul5@gmail.com">
+          <Inbox className="inline-flex items-center w-4 h-4 ml-1 mb-1 mr-1" />{" "}
+          Email
+        </a>
         <br />
         <a
           href="https://github.com/avirajpaul5"
@@ -11,6 +16,7 @@ export default function Contact() {
           rel="noopener noreferrer"
         >
           🔗 GitHub
+          <ExternalLink className="inline-flex items-center w-4 h-4 ml-1 mb-1" />
         </a>
         <br />
         <a
@@ -19,6 +25,7 @@ export default function Contact() {
           rel="noopener noreferrer"
         >
           💼 LinkedIn
+          <ExternalLink className="inline-flex items-center w-4 h-4 ml-1 mb-1" />
         </a>
         <br />
       </div>
