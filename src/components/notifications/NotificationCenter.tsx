210f72b1169d9ce4a4ef5e8018b9abf0b37c25f1
Aviraj Paul <96339559+avirajpaul5@users.noreply.github.com>
2025-04-11 07:57:25 +0530

    Minor cleanup for readability

--- src/components/notifications/NotificationCenter.tsx
+++ src/components/notifications/NotificationCenter.tsx
@@ -87,13 +87,14 @@ export default function NotificationCenter({
           <WeatherWidget />
           <div style={{ height: "360px" }}>
             <iframe
-              title="Spotify Embed: Recommendation Playlist"
               src={`https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0`}
+              title="Spotify Embed: Recommendation Playlist"
               width="100%"
               height="100%"
               frameBorder="0"
               allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
               loading="lazy"
+              className="spotify-playlist"
             />
           </div>
         </div>
